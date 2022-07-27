57d49ad4778a47eaa8651676638bd8160d7c9b97
Subhasmith <40554910+subhasmiththapa2502@users.noreply.github.com>
2022-07-27 23:56:37 +0400

    image loader in Home screen
    Add Loader in home screen

--- app/src/main/java/com/yolo/test/presentation/home/HomeFragment.java
+++ app/src/main/java/com/yolo/test/presentation/home/HomeFragment.java
@@ -5,6 +5,7 @@ import static com.yolo.test.common.Constants.STATIC_COLLAPSING_TOOLBAR_IMAGE;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkRequest;
@@ -27,7 +28,11 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.material.snackbar.Snackbar;
 import com.thecode.aestheticdialogs.AestheticDialog;
 import com.thecode.aestheticdialogs.DialogAnimation;
@@ -114,9 +119,25 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     }
 
     private void showImage() {
+        fragmentHomeBinding.lottieLoader1.setVisibility(View.VISIBLE);
         Glide.with(requireActivity())
                 .load(STATIC_COLLAPSING_TOOLBAR_IMAGE)
                 .transition(DrawableTransitionOptions.withCrossFade())
+                .addListener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        fragmentHomeBinding.lottieLoader1.setVisibility(View.GONE);
+                        showSnackBar(getString(R.string.error_loading_image));
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        fragmentHomeBinding.lottieLoader1.setVisibility(View.GONE);
+                        return false;
+
+                    }
+                })
                 .into(fragmentHomeBinding.expandedImage);
     }
 
